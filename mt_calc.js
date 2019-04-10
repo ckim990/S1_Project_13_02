50bb66a61cc7046e758dcb5fe9634c9d14cbddd8
Chris Kim <ckim990@west-mec.org>
2019-04-09 21:22:08 -0700

    Changed names of files and added links

---
+++ mt_calc.js
@@ -0,0 +1,42 @@
+"use strict";
+
+/*
+   New Perspectives on HTML5, CSS3 and JavaScript 6th Edition
+   Tutorial 11
+   Case Problem 2
+
+   Author: Christopher Kim
+   Date:   3.15.19
+   
+   Filename: mt_calc.js
+	
+   Functions List:
+
+   init()
+      Initializes the contents of the web page and sets up the
+      event handlers
+      
+   buttonClick(e)
+      Adds functions to the buttons clicked within the calcutlor
+      
+   calcKeys(e)
+      Adds functions to key pressed within the calculator window 
+      
+   eraseChar(textStr)
+      Erases the last character from the text string, textStr
+      
+   evalEq(textStr, decimals) 
+      Evaluates the equation in textStr, returning a value to the number of decimals specified by the decimals parameter
+
+   lastEq(textStr) 
+      Returns the previous expression from the list of expressions in the textStr parameter
+
+*/
+
+
+
+
+
+
+/* ===================================================================== */
+
